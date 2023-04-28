3696f7261c53f8bee3ca2740b8f43b5e121115a6
Samuel Rivera <114023422+SamuelRiveraB@users.noreply.github.com>
2023-04-28 17:20:43 -0500

    Restart Project

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Socials from './Socials.svelte';
+</script>
+
+<style lang="scss">
+    header {
+        display: flex;
+        position: fixed;
+        width: 100%;
+        height: 50px;
+        top: 0;
+        left: 0;
+        align-items: center;
+        justify-content: center;
+        background-color: #fff;
+        border-bottom: 1px solid #ccc;
+    }
+</style>
+
+<header class="header">
+    <Socials />
+</header>
